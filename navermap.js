4d0056a1083954045b7a90a1ea5f3e60f4978c0c
YeongminKim <oinochoe@naver.com>
2021-08-06 01:15:56 +0900

    지도 검색 테스트

--- navermap.js
+++ navermap.js
@@ -118,17 +118,51 @@ var _naverMap = (function (naverMap) {
     naverMap.init = function () {
         naverMap.event();
         naverMap.render();
+        const autoCompleteJS = new autoComplete({
+            selector: '#autoComplete',
+            placeHolder: 'Search for Food...',
+            data: {
+                src: ['Sauce - Thousand Island', 'Wild Boar - Tenderloin', 'Goat - Whole Cut'],
+                cache: true,
+            },
+            resultsList: {
+                element: (list, data) => {
+                    if (!data.results.length) {
+                        // Create "No Results" message element
+                        const message = document.createElement('div');
+                        // Add class to the created element
+                        message.setAttribute('class', 'no_result');
+                        // Add message text content
+                        message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+                        // Append message element to the results list
+                        list.prepend(message);
+                    }
+                },
+                noResults: true,
+            },
+            resultItem: {
+                highlight: true,
+            },
+            events: {
+                input: {
+                    selection: (event) => {
+                        const selection = event.detail.selection.value;
+                        autoCompleteJS.input.value = selection;
+                        var location = new naver.maps.LatLng(37.2810716, 127.0423522);
+
+                        map.setCenter(location);
+                    },
+                },
+            },
+        });
     };
 
     naverMap.event = function () {
         $('.buttons > input').on('click', function (e) {
             e.preventDefault();
-
             var mapTypeId = this.id;
-
             if (map.getMapTypeId() !== naver.maps.MapTypeId[mapTypeId]) {
                 map.setMapTypeId(naver.maps.MapTypeId[mapTypeId]); // 지도 유형 변경하기
-
                 $('.buttons > input').removeClass('control-on');
                 $(this).addClass('control-on');
             }
